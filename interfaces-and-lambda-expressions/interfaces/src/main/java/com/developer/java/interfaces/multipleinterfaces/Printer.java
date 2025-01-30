5987e282738fae2c85dc03d831b0396b2df5c3fc
leonelmfonseca <leonelfonseca@gmail.com>
2025-01-30 16:11:44 +0000

    feat(interfaces): implementing multiple interfaces

---
+++ interfaces-and-lambda-expressions/interfaces/src/main/java/com/developer/java/interfaces/multipleinterfaces/Printer.java
@@ -0,0 +1,5 @@
+package com.developer.java.interfaces.multipleinterfaces;
+
+public interface Printer {
+    void printDoc(String document);
+}
---
+++ interfaces-and-lambda-expressions/interfaces/src/main/java/com/developer/java/interfaces/multipleinterfaces/PrinterWithScanner.java
@@ -0,0 +1,23 @@
+package com.developer.java.interfaces.multipleinterfaces;
+
+import static com.developer.java.output.Printing.print;
+
+public class PrinterWithScanner implements Printer , Scanner{
+
+    public static void main(String[] args){
+        PrinterWithScanner printer = new PrinterWithScanner();
+        printer.scan();
+        printer.printDoc("secret document");
+    }
+
+    @Override
+    public void printDoc(String document) {
+        print("As a multifunction printer, I'll use my capabilities to print");
+    }
+
+    @Override
+    public void scan() {
+        print("I want to capture the information on paper and translating it into a digital file that can be stored, edited, or shared on a computer or other devices");
+    }
+
+}
