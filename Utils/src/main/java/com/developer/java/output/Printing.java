30f1b7649484cf1ff1102047decd658f58ea002d
leonelmfonseca <leonelfonseca@gmail.com>
2025-01-31 14:52:04 +0000

    feat(utils): add print funcs

---
+++ Utils/src/main/java/com/developer/java/output/Printing.java
@@ -0,0 +1,32 @@
+package com.developer.java.output;
+
+import java.util.Arrays;
+
+public class Printing {
+
+  private static final int DEFAULT_LINEBREAK_COUNT = 1;
+
+  public static void print(String message, int aboveLnBreaks, int belowLnBreaks) {
+    String builder =
+        String.valueOf(System.lineSeparator()).repeat(Math.max(0, aboveLnBreaks))
+            + message
+            + String.valueOf(System.lineSeparator()).repeat(Math.max(0, belowLnBreaks));
+    print(builder);
+  }
+
+  public static void printOneLine(String name, String message) {
+    print(name + ": " + message );
+  }
+  public static void print(String message) {
+    System.out.println(message);
+  }
+
+  public static void printLn(String message) {
+    print(message, DEFAULT_LINEBREAK_COUNT, DEFAULT_LINEBREAK_COUNT);
+  }
+
+  public static void printLn(String... strings) {
+    Arrays.stream(strings).forEach(Printing::printLn);
+  }
+
+}
